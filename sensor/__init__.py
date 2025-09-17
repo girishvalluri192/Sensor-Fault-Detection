af81ceba860e0aea912e4ea618daf94c031d9649
girishvalluri192 <girishvalluri192@gmail.com>
2025-09-17 19:03:09 +0530

    strucyure setup done

---
+++ sensor/__init__.py
@@ -0,0 +1,4 @@
+# Inorder to convert any folder into python package 
+# we need to create __init__.py file inside that folder
+# Now that folder becomes the package
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup
+from typing import List
+
+setup(
+    
+    name="sensor",
+    version="0.0.1",
+    author="girish",
+    author_email="girishvalluri192@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
+if __name__ == "__main__":
+    setup.py
+    
